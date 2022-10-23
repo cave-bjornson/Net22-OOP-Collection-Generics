6bd17a3d035eb23037b004973d26c04f5c8d81c2
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-24 00:23:44 +0200

    feat: add employee class

---
+++ ConsoleApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+    public decimal Salary { get; set; }
+
+    public Employee(int id, string name, Gender gender, decimal salary)
+    {
+        Id = id;
+        Name = name;
+        Gender = gender;
+        Salary = salary;
+    }
+
+    protected bool Equals(Employee other)
+    {
+        return Id == other.Id;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Gender)}: {Gender}, {nameof(Salary)}: {Salary}";
+    }
+}
+
+public enum Gender
+{
+    Male,
+    Female
+}
